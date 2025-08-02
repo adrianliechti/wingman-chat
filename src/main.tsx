ab77f5bfecddd98f0d4e58778f44614d2a3e5834
Adrian Liechti <adrian@monobox.ch>
2025-08-02 12:11:39 +0200

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import "./index.css";
 import App from "./App.tsx";
 
 import { loadConfig } from "./config.ts";
+import { initTelemetry } from "./lib/telemetry";
 
 const bootstrap = async () => {
   try {
+    // Initialize telemetry before anything else
+    initTelemetry();
+    
     const config = await loadConfig();
 
     if (config?.title) {
