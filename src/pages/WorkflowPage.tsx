8c52e31d688cd5a07050b0500aadb005b358a164
Adrian Liechti <adrian@monobox.ch>
2025-10-22 16:39:30 +0200

    allow to clear flow

--- src/pages/WorkflowPage.tsx
+++ src/pages/WorkflowPage.tsx
@@ -1,14 +1,41 @@
+import { useEffect } from 'react';
+import { Plus as PlusIcon } from 'lucide-react';
+import { Button } from '@headlessui/react';
 import { WorkflowProvider } from '../contexts/WorkflowProvider';
 import { WorkflowPalette } from '../components/WorkflowPalette';
 import { WorkflowCanvas } from '../components/WorkflowCanvas';
+import { useNavigation } from '../hooks/useNavigation';
+import { useWorkflow } from '../hooks/useWorkflow';
+
+function WorkflowPageContent() {
+  const { setRightActions } = useNavigation();
+  const { clearWorkflow } = useWorkflow();
+
+  useEffect(() => {
+    setRightActions(
+      <>
+        <Button
+          className="p-2 text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-200 rounded transition-all duration-150 ease-out"
+          onClick={clearWorkflow}
+        >
+          <PlusIcon size={20} />
+        </Button>
+      </>
+    );
+  }, [setRightActions, clearWorkflow]);
+
+  return (
+    <div className="h-full w-full flex overflow-hidden relative">
+      <WorkflowCanvas />
+      <WorkflowPalette />
+    </div>
+  );
+}
 
 export function WorkflowPage() {
   return (
     <WorkflowProvider>
-      <div className="h-full w-full flex overflow-hidden relative">
-        <WorkflowCanvas />
-        <WorkflowPalette />
-      </div>
+      <WorkflowPageContent />
     </WorkflowProvider>
   );
 }
